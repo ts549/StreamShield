4b9c96c92233ac3d15bb88085971d9b784c9dd18
ts549 <sun955@purdue.edu>
2025-09-23 22:30:28 -0400

    Trained official detection model for all categories

--- undox-security-server/models/detection_model.py
+++ undox-security-server/models/detection_model.py
@@ -5,18 +5,30 @@ class DetectionModel:
     def __init__(self, weights="yolov8n.pt"):
         self.model = YOLO(weights)
 
-    def train(self, data, epochs=50, imgsz=640, batch=16, device=0, patience=20):
-        results = self.model.train(data=data, epochs=epochs, imgsz=imgsz, batch=batch, device=device, patience=patience)
+    def train(self, data, project, name, epochs=50, imgsz=640, batch=16, device="cpu", patience=20):
+        results = self.model.train(data=data, project=project, name=name, epochs=epochs, imgsz=imgsz, batch=batch, device=device, patience=patience)
         return results
 
-    def evaluate(self):
-        metrics = self.model.val()
+    def evaluate(self, data):
+        metrics = self.model.val(data)
         return metrics
 
-    def predict(self, source, save=True, save_txt=True, imgsz=640, conf=0.25):
-        results = self.model.predict(source=source, save=save, save_txt=save_txt, imgsz=imgsz, conf=conf)
+    def predict(self, source, project, save=True, save_txt=True, imgsz=640, conf=0.25):
+        results = self.model.predict(source=source, project=project, save=save, save_txt=save_txt, imgsz=imgsz, conf=conf)
         return results
 
 
 if (__name__ == "__main__"):
-    pass
+    data = "./datasets/all_sensitive_info/data.yaml"
+    test_source = "./datasets/all_sensitive_info/test/images"
+    project = "./runs/train"
+    name = "finetuned"
+
+    detect = DetectionModel(weights="./runs/train/finetuned/weights/best.pt")
+
+    # detect.train(data=data, project=project, name=name)
+
+    # detect.evaluate(data=data, project=project)
+
+    project = "./runs/predicts"
+    detect.predict(source=test_source, project=project)
